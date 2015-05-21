2f3c5d2d9585270f9c66e502c2edb0b6c5bd8192
Florian Weber <florian.weber.dd@icloud.com>
2015-05-21 05:47:23 +0000

    fix dev env send error

--- EventListener/ExceptionListener.php
+++ EventListener/ExceptionListener.php
@@ -34,7 +34,7 @@ class ExceptionListener
 
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
-        if ($this->environment != 'prod' && is_null($this->trackerEndpoint)) {
+        if ($this->environment != 'prod' || is_null($this->trackerEndpoint)) {
             return;
         }
 
