b8251cbd529c0cf52fc03bfc425a182fde63f6cd
fweber-de <florian.weber.dd@icloud.com>
2016-01-08 11:45:43 +0100

    trace

--- EventListener/ExceptionListener.php
+++ EventListener/ExceptionListener.php
@@ -4,7 +4,6 @@ namespace Ligneus\ExceptionTrackerBundle\EventListener;
 
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpFoundation\Request;
-use Ligneus\ExceptionTrackerBundle\Exception\ExplanatoryException;
 
 /**
  * @author Florian Weber <fweber@ligneus.de>
@@ -35,9 +34,9 @@ class ExceptionListener
 
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
-        if ($this->environment != 'prod' || is_null($this->trackerEndpoint)) {
-            return;
-        }
+        // if ($this->environment != 'prod' || is_null($this->trackerEndpoint)) {
+        //     return;
+        // }
 
         $this->logException($event->getException(), $event->getRequest());
 
@@ -57,7 +56,7 @@ class ExceptionListener
         $data['class'] = get_class($exception);
         $data['uri'] = $request->getRequestUri();
         $data['ip'] = $request->getClientIp();
-        $data['trace'] = $exception->getTraceAsString();
+        $data['trace'] = $this->getExceptionTraceAsString($exception);
 
         $data['title'] = '';
         $data['text'] = '';
@@ -82,5 +81,46 @@ class ExceptionListener
         );
 
         return curl_exec($ch);
+    }
+
+    protected function getExceptionTraceAsString($exception)
+    {
+        $rtn = "";
+        $count = 0;
+        foreach ($exception->getTrace() as $frame) {
+            $args = "";
+            if (isset($frame['args'])) {
+                $args = array();
+                foreach ($frame['args'] as $arg) {
+                    if (is_string($arg)) {
+                        $args[] = "'" . $arg . "'";
+                    } elseif (is_array($arg)) {
+                        $args[] = "Array";
+                    } elseif (is_null($arg)) {
+                        $args[] = 'NULL';
+                    } elseif (is_bool($arg)) {
+                        $args[] = ($arg) ? "true" : "false";
+                    } elseif (is_object($arg)) {
+                        $args[] = get_class($arg);
+                    } elseif (is_resource($arg)) {
+                        $args[] = get_resource_type($arg);
+                    } else {
+                        $args[] = $arg;
+                    }
+                }
+                $args = join(", ", $args);
+            }
+            $rtn .= sprintf(
+                "#%s %s(%s): %s(%s)\n",
+                $count,
+                isset($frame['file']) ? $frame['file'] : 'unknown file',
+                isset($frame['line']) ? $frame['line'] : 'unknown line',
+                (isset($frame['class']))  ? $frame['class'].$frame['type'].$frame['function'] : $frame['function'],
+                $args
+            );
+            $count++;
+        }
+
+        return $rtn;
     }
 }
